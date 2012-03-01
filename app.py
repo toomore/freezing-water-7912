f73dc96fed5a7c61b7e78bb396c4e51f9ddb5cd2
Toomore Chiang <toomore0929@gmail.com>
2012-03-01 14:43:03 +0800

    Add worker

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'Hello World!!!!!!!!!!!!!'
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
---
+++ apps.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import urllib2
+
+urllib2.urlopen('http://plurkii.appspot.com/')
