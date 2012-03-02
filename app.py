0b58d18b98ceccce374d46b16f09f044607bbee7
Toomore Chiang <toomore0929@gmail.com>
2012-03-02 10:14:12 +0800

    Test grs

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from fetch_data import grs_stock
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/g')
-def gg():
-    g = grs_stock(2618)
+@app.route('/g/<int:no>')
+def gg(no):
+    g = grs_stock(no)
     op = g.serial_price(6)
-    return str(op)
+    return str(g.row_data)
 
 @app.route('/')
 def hello():
@@ -21,4 +21,4 @@ def hello():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port,debug=True)
