43181ed6dd6374fe4f797145667cbe142199e358
Toomore Chiang <toomore0929@gmail.com>
2012-03-09 14:24:18 +0800

    Test tasks.

--- rttasks.py
+++ rttasks.py
@@ -3,9 +3,13 @@
 from fabric.api import env, hosts, roles, run
 import os
 
+print os.environ.get('SCHEDULER_URL')
+
+'''
 @hosts(os.environ.get('SCHEDULER_URL'))
 @roles('OOPP')
 def ff():
     run('python ./rttasks1.py')
 
 ff()
+'''
