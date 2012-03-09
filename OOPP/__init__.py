0ca9fa853b1adb17c0c1eecdc3747703953bae80
Toomore Chiang <toomore0929@gmail.com>
2012-03-09 15:10:59 +0800

    Test Tasks.

---
+++ OOPP/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from fabric.api import env, hosts, roles, run
+import os
+
+#print os.environ.get('SCHEDULER_URL')
+
+@hosts(os.environ.get('SCHEDULER_URL'))
+@roles('OOPP')
+def ff():
+    #run('python rttasks1.py')
+    print '123'
+
+ff()
